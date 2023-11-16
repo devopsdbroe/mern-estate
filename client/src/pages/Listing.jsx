6ffb18b44e19a5b3e0076f34d522d0defad4cf05
devopsdbroe <dbroe1995@gmail.com>
2023-11-15 20:52:17 -0500

    Added contact functionality to listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
+import { useSelector } from "react-redux";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
 import {
@@ -13,13 +14,16 @@ import {
 	FaParking,
 	FaShare,
 } from "react-icons/fa";
+import Contact from "../components/Contact";
 
 const Listing = () => {
+	const { currentUser } = useSelector((state) => state.user);
 	SwiperCore.use([Navigation]);
 	const [listing, setListing] = useState(null);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
 	const [copied, setCopied] = useState(false);
+	const [contact, setContact] = useState(false);
 	const params = useParams();
 
 	useEffect(() => {
@@ -132,6 +136,15 @@ const Listing = () => {
 								{listing.furnished ? "Furnished" : "Unfurnished"}
 							</li>
 						</ul>
+						{currentUser && listing.userRef !== currentUser._id && !contact && (
+							<button
+								onClick={() => setContact(true)}
+								className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+							>
+								Contact Landlord
+							</button>
+						)}
+						{contact && <Contact listing={listing} />}
 					</div>
 				</>
 			)}
