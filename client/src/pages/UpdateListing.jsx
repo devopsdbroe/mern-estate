5c10e024f4661d4c4d4b3246d1273338bccebb62
devopsdbroe <dbroe1995@gmail.com>
2023-11-19 11:09:58 -0500

    Created listing card component and show listings

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -375,7 +375,7 @@ const CreateListing = () => {
 						disabled={loading || uploading}
 						className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
 					>
-						{loading ? "Creating..." : "Edit Listing"}
+						{loading ? "Updating..." : "Edit Listing"}
 					</button>
 					{error && <p className="text-red-700 text-sm">{error}</p>}
 				</div>
